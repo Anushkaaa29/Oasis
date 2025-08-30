7edb66f1603ab0ab39b77bf9d3b16b6bf2d4e3e9
Anushka <anushkapandeyy27@gmail.com>
2025-08-30 15:24:25 +0530

    Add files via upload

---
+++ tribute lata mangeshkar/script.js
@@ -0,0 +1,21 @@
+// Reveal sections on scroll
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".fade-in");
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+});
